78c69f7f920ec02bcc97ec7ea947e91d376dfedf
rieperriton <d00500068@utahtech.edu>
2025-04-06 15:54:54 -0600

    Update all_tests.py

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -1,4 +1,4 @@
 import subprocess
 
 # Run all tests. Please add your test files under the next line in the format of the next line.
-subprocess.run(["python", "tests/test_login.py"])
+subprocess.run(["python", "tests/test_login.py", "tests/settingstest2.py"])
